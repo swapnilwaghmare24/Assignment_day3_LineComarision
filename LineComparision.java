d8066497a59d899293797bc7377a5b9304620ab1
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-07-02 18:03:49 +0530

    get coordinates of lines and lenth of lines calculated

--- LineComparision.java
+++ LineComparision.java
@@ -1,8 +1,38 @@
-
+import java.util.Scanner;
+import static java.lang.Math.*;
 public class LineComparision
 {
+	Scanner sc = new Scanner(System.in);
+	int x1,y1,x2,y2;
+	void getCoordinates()
+	{
+		System.out.println("Enter starting co-ordinates of line");
+		x1=sc.nextInt();
+		y1=sc.nextInt();
+		System.out.println("Enter ending co-ordinates of line");
+		x2=sc.nextInt();
+		y2=sc.nextInt();
+		
+	}
+	
+	String calLength()
+	{
+		
+		String length=String.valueOf(Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)));
+		return length;
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to line comparision computation program");
+		LineComparision lc = new LineComparision();
+		System.out.println("Enter co-ordinates for line1");
+		lc.getCoordinates();
+		String length1=lc.calLength();
+		System.out.println("Length of line1 is "+length1);
+		System.out.println("Enter co-ordinates for line2");
+		lc.getCoordinates();
+		String length2=lc.calLength();
+		System.out.println("Length of line1 is "+length2);
+		
 	}
 }
